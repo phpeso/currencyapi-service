b3e9126f6123ffbec569ac205173f0d09be95b29
Anton Smirnov <sandfox+git@sandfox.me>
2025-07-30 01:18:56 +0300

    Test current

---
+++ tests/CurrentRateTest.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Peso\Services\Tests;
+
+use Arokettu\Date\Calendar;
+use Peso\Core\Exceptions\ExchangeRateNotFoundException;
+use Peso\Core\Requests\CurrentExchangeRateRequest;
+use Peso\Core\Responses\ErrorResponse;
+use Peso\Core\Responses\ExchangeRateResponse;
+use Peso\Services\CurrencyApiService;
+use Peso\Services\Tests\Helpers\MockClient;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Psr16Cache;
+
+final class CurrentRateTest extends TestCase
+{
+    public function testRate(): void
+    {
+        $cache = new Psr16Cache(new ArrayAdapter());
+        $http = MockClient::get();
+
+        $service = new CurrencyApiService('xxxfreexxx', cache: $cache, httpClient: $http);
+        $today = Calendar::parse('2025-07-28');
+
+        $response = $service->send(new CurrentExchangeRateRequest('EUR', 'USD'));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('1.1598235485', $response->rate->value);
+        self::assertEquals($today, $response->date->toString());
+
+        $response = $service->send(new CurrentExchangeRateRequest('USD', 'RUB'));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('81.3219983351', $response->rate->value);
+        self::assertEquals($today, $response->date->toString());
+
+        $response = $service->send(new CurrentExchangeRateRequest('RUB', 'JPY'));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('1.8258420785', $response->rate->value);
+        self::assertEquals($today, $response->date->toString());
+
+        $response = $service->send(new CurrentExchangeRateRequest('EUR', 'JPY')); // cached
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('172.2119069792', $response->rate->value);
+        self::assertEquals($today, $response->date->toString());
+
+        self::assertCount(3, $http->getRequests()); // subsequent requests are cached
+    }
+
+    public function testRateWithSymbols(): void
+    {
+        $cache = new Psr16Cache(new ArrayAdapter());
+        $http = MockClient::get();
+
+        $service = new CurrencyApiService('xxxfreexxx', symbols: [
+            'EUR', 'USD',
+        ], cache: $cache, httpClient: $http);
+        $today = Calendar::parse('2025-07-28');
+
+        $response = $service->send(new CurrentExchangeRateRequest('EUR', 'USD'));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('1.1598235485', $response->rate->value);
+        self::assertEquals($today, $response->date->toString());
+
+        $response = $service->send(new CurrentExchangeRateRequest('USD', 'EUR'));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('0.8622001177', $response->rate->value);
+        self::assertEquals($today, $response->date->toString());
+
+        // any to symbols is ok
+        $response = $service->send(new CurrentExchangeRateRequest('RUB', 'EUR'));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('0.0106022987', $response->rate->value);
+        self::assertEquals($today, $response->date->toString());
+
+        // symbols to missing is not OK
+        $response = $service->send(new CurrentExchangeRateRequest('EUR', 'RUB'));
+        self::assertInstanceOf(ErrorResponse::class, $response);
+        self::assertInstanceOf(ExchangeRateNotFoundException::class, $response->exception);
+        self::assertEquals('Unable to find exchange rate for EUR/RUB', $response->exception->getMessage());
+    }
+
+    public function testInvalidCurrency(): void
+    {
+        $cache = new Psr16Cache(new ArrayAdapter());
+        $http = MockClient::get();
+
+        $service = new CurrencyApiService('xxxfreexxx', cache: $cache, httpClient: $http);
+
+        $response = $service->send(new CurrentExchangeRateRequest('XBT', 'USD'));
+        self::assertInstanceOf(ErrorResponse::class, $response);
+        self::assertInstanceOf(ExchangeRateNotFoundException::class, $response->exception);
+        self::assertEquals('Unable to find exchange rate for XBT/USD', $response->exception->getMessage());
+    }
+}
---
+++ tests/Helpers/MockClient.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Peso\Services\Tests\Helpers;
+
+use GuzzleHttp\Psr7\Response;
+use Http\Message\RequestMatcher\RequestMatcher;
+use Http\Mock\Client;
+use Psr\Http\Message\RequestInterface;
+
+final readonly class MockClient
+{
+    public static function get(): Client
+    {
+        $client = new Client();
+
+        $client->on(
+            new RequestMatcher('/v3/latest', 'api.currencyapi.com', ['GET'], ['https']),
+            static function (RequestInterface $request) {
+                $query = $request->getUri()->getQuery();
+                switch ($query) {
+                    case 'apikey=xxxfreexxx&base_currency=EUR':
+                        return new Response(body: fopen(__DIR__ . '/../data/latest-EUR.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&base_currency=USD':
+                        return new Response(body: fopen(__DIR__ . '/../data/latest-USD.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&base_currency=RUB':
+                        return new Response(body: fopen(__DIR__ . '/../data/latest-RUB.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&base_currency=EUR&currencies=EUR%2CUSD':
+                        return new Response(body: fopen(__DIR__ . '/../data/latest-EUR-EUR,USD.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&base_currency=USD&currencies=EUR%2CUSD':
+                        return new Response(body: fopen(__DIR__ . '/../data/latest-USD-EUR,USD.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&base_currency=RUB&currencies=EUR%2CUSD':
+                        return new Response(body: fopen(__DIR__ . '/../data/latest-RUB-EUR,USD.json', 'r'));
+
+                    case 'apikey=rate_exceeded&base_currency=TRY':
+                        return new Response(429, body: fopen(__DIR__ . '/../data/rate-limit.json', 'r'));
+
+                    case 'apikey=invalid&base_currency=TRY':
+                        return new Response(401, body: fopen(__DIR__ . '/../data/invalid-key.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&base_currency=XBT':
+                        return new Response(422, body: fopen(__DIR__ . '/../data/invalid-base.json', 'r'));
+
+                    default:
+                        throw new \LogicException('Non-mocked query: ' . $query);
+                }
+            },
+        );
+        $client->on(
+            new RequestMatcher('/v3/historical', 'api.currencyapi.com', ['GET'], ['https']),
+            static function (RequestInterface $request) {
+                $query = $request->getUri()->getQuery();
+                switch ($query) {
+                    case 'apikey=xxxfreexxx&date=2025-06-13&base_currency=EUR':
+                        return new Response(body: fopen(__DIR__ . '/../data/2025-06-13-EUR.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&date=2025-06-13&base_currency=USD':
+                        return new Response(body: fopen(__DIR__ . '/../data/2025-06-13-USD.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&date=2025-06-13&base_currency=RUB':
+                        return new Response(body: fopen(__DIR__ . '/../data/2025-06-13-RUB.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&date=2025-06-13&base_currency=EUR&currencies=EUR%2CUSD':
+                        return new Response(body: fopen(__DIR__ . '/../data/2025-06-13-EUR-EUR,USD.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&date=2025-06-13&base_currency=USD&currencies=EUR%2CUSD':
+                        return new Response(body: fopen(__DIR__ . '/../data/2025-06-13-USD-EUR,USD.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&date=2025-06-13&base_currency=RUB&currencies=EUR%2CUSD':
+                        return new Response(body: fopen(__DIR__ . '/../data/2025-06-13-RUB-EUR,USD.json', 'r'));
+
+                    case 'apikey=xxxfreexxx&date=2025-06-13&base_currency=XBT':
+                        return new Response(422, body: fopen(__DIR__ . '/../data/invalid-base.json', 'r'));
+
+                    default:
+                        throw new \LogicException('Non-mocked query: ' . $query);
+                }
+            },
+        );
+
+        return $client;
+    }
+}
