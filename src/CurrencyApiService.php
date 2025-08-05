b1a1ab325451064fb7f782841719e6555a7d8528
Anton Smirnov <sandfox+git@sandfox.me>
2025-08-06 01:23:57 +0300

    Conversion

--- src/CurrencyApiService.php
+++ src/CurrencyApiService.php
@@ -5,13 +5,18 @@ declare(strict_types=1);
 namespace Peso\Services;
 
 use Arokettu\Date\Calendar;
+use Closure;
 use DateInterval;
 use Error;
 use Override;
+use Peso\Core\Exceptions\ConversionNotPerformedException;
 use Peso\Core\Exceptions\ExchangeRateNotFoundException;
 use Peso\Core\Exceptions\RequestNotSupportedException;
+use Peso\Core\Requests\CurrentConversionRequest;
 use Peso\Core\Requests\CurrentExchangeRateRequest;
+use Peso\Core\Requests\HistoricalConversionRequest;
 use Peso\Core\Requests\HistoricalExchangeRateRequest;
+use Peso\Core\Responses\ConversionResponse;
 use Peso\Core\Responses\ErrorResponse;
 use Peso\Core\Responses\ExchangeRateResponse;
 use Peso\Core\Services\PesoServiceInterface;
@@ -24,17 +29,21 @@ use Peso\Core\Types\Decimal;
 use Peso\Services\CurrencyApiService\Subscription;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\SimpleCache\CacheInterface;
 
 final readonly class CurrencyApiService implements PesoServiceInterface
 {
     private const LATEST_ENDPOINT = 'https://api.currencyapi.com/v3/latest?';
     private const HISTORICAL_ENDPOINT = 'https://api.currencyapi.com/v3/historical?';
+    private const CONVERT_ENDPOINT = 'https://api.currencyapi.com/v3/convert?';
 
     public function __construct(
         private string $apiKey,
         private Subscription $subscription,
         private array|null $symbols = null,
+        private bool $multiconversion = false,
         private CacheInterface $cache = new NullCache(),
         private DateInterval $ttl = new DateInterval('PT1H'),
         private ClientInterface $httpClient = new DiscoveredHttpClient(),
@@ -43,10 +52,13 @@ final readonly class CurrencyApiService implements PesoServiceInterface
     }
 
     #[Override]
-    public function send(object $request): ExchangeRateResponse|ErrorResponse
+    public function send(object $request): ExchangeRateResponse|ConversionResponse|ErrorResponse
     {
         if ($request instanceof CurrentExchangeRateRequest || $request instanceof HistoricalExchangeRateRequest) {
             return self::performRateRequest($request);
+        }
+        if ($request instanceof CurrentConversionRequest || $request instanceof HistoricalConversionRequest) {
+            return self::performConversionRequest($request);
         }
         return new ErrorResponse(RequestNotSupportedException::fromRequest($request));
     }
@@ -67,7 +79,15 @@ final readonly class CurrencyApiService implements PesoServiceInterface
             $url = self::HISTORICAL_ENDPOINT . http_build_query($query, encoding_type: PHP_QUERY_RFC3986);
         }
 
-        $rates = $this->retrieveRates($url, $request);
+        $rates = $this->retrieveRates($url, static fn (
+            RequestInterface $httpRequest,
+            ResponseInterface $httpResponse,
+        ) => new ErrorResponse(
+            ExchangeRateNotFoundException::fromRequest(
+                $request,
+                HttpFailureException::fromResponse($httpRequest, $httpResponse),
+            ),
+        ));
 
         if ($rates instanceof ErrorResponse) {
             return $rates;
@@ -87,10 +107,65 @@ final readonly class CurrencyApiService implements PesoServiceInterface
         return new ExchangeRateResponse(Decimal::init($rate), $date);
     }
 
-    private function retrieveRates(
-        string $url,
-        CurrentExchangeRateRequest|HistoricalExchangeRateRequest $pesoRequest,
-    ): array|ErrorResponse {
+    private function performConversionRequest(
+        CurrentConversionRequest|HistoricalConversionRequest $request,
+    ): ErrorResponse|ConversionResponse {
+        if ($this->subscription !== Subscription::Paid) {
+            return new ErrorResponse(RequestNotSupportedException::fromRequest($request));
+        }
+
+        $query = [
+            'apikey' => $this->apiKey,
+            'base_currency' => $request->baseCurrency,
+            'value' => $request->baseAmount->value,
+        ];
+
+        if ($this->multiconversion) {
+            // convert all currencies or selected symbols
+            $query['currencies'] = $this->symbols === null ? null : implode(',', $this->symbols);
+        } else {
+            $query['currencies'] = $request->quoteCurrency;
+        }
+
+        if ($request instanceof HistoricalConversionRequest) {
+            $query['date'] = $request->date->toString();
+        }
+
+        $url = self::CONVERT_ENDPOINT . http_build_query($query, encoding_type: PHP_QUERY_RFC3986);
+
+        $amounts = $this->retrieveRates($url, static fn (
+            RequestInterface $httpRequest,
+            ResponseInterface $httpResponse,
+        ) => new ErrorResponse(
+            ConversionNotPerformedException::fromRequest(
+                $request,
+                HttpFailureException::fromResponse($httpRequest, $httpResponse),
+            ),
+        ));
+
+        if ($amounts instanceof ErrorResponse) {
+            return $amounts;
+        }
+
+        $amount = $amounts['data'][$request->quoteCurrency]['value'] ?? null;
+
+        if ($amount === null) {
+            return new ErrorResponse(ConversionNotPerformedException::fromRequest($request));
+        }
+
+        $date = Calendar::parseDateTimeString(
+            $amounts['meta']['last_updated_at'] ??
+            throw new Error('Unexpected response: last_updated_at missing'),
+        );
+
+        return new ConversionResponse(Decimal::init($amount), $date);
+    }
+
+    /**
+     * @psalm-param Closure(RequestInterface, ResponseInterface): ErrorResponse $errorResponse
+     */
+    private function retrieveRates(string $url, Closure $errorResponse): array|ErrorResponse
+    {
         $cacheKey = 'peso|curapi|' . hash('sha1', $url);
 
         $rates = $this->cache->get($cacheKey);
@@ -109,12 +184,7 @@ final readonly class CurrencyApiService implements PesoServiceInterface
 
         if ($response->getStatusCode() === 422) {
             // do not throw
-            return new ErrorResponse(
-                ExchangeRateNotFoundException::fromRequest(
-                    $pesoRequest,
-                    HttpFailureException::fromResponse($request, $response),
-                ),
-            );
+            return $errorResponse($request, $response);
         }
         if ($response->getStatusCode() !== 200) {
             throw HttpFailureException::fromResponse($request, $response);
@@ -133,7 +203,14 @@ final readonly class CurrencyApiService implements PesoServiceInterface
     #[Override]
     public function supports(object $request): bool
     {
-        // TODO: paid also supports Conversion but I don't have any access for now
-        return $request instanceof CurrentExchangeRateRequest || $request instanceof HistoricalExchangeRateRequest;
+        if ($request instanceof CurrentExchangeRateRequest || $request instanceof HistoricalExchangeRateRequest) {
+            return true;
+        }
+
+        if ($request instanceof CurrentConversionRequest || $request instanceof HistoricalConversionRequest) {
+            return $this->subscription === Subscription::Paid;
+        }
+
+        return false;
     }
 }
